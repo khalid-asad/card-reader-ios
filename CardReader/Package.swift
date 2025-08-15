362bdf0f49055f7fe42719373063f99bf0d593c1
Khalid Asad <khalidasad93@gmail.com>
2025-08-15 13:50:55 -0400

    Update minimum version to iOS 18, Swift 6.0.

--- CardReader/Package.swift
+++ CardReader/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CardReader",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v18)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
